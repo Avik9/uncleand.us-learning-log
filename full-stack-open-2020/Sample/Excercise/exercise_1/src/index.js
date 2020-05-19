fac0e6c3b0d3a243c469894112d9f6398403f96c
Avik Kadakia <avik.kadakia@stonybrook.edu>
2020-05-19 19:24:09 -0400

    Finished 1b

--- full-stack-open-2020/Sample/Excercise/exercise_1/src/index.js
+++ full-stack-open-2020/Sample/Excercise/exercise_1/src/index.js
@@ -4,18 +4,19 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
   return (
     <h1>
-      {props.name}
+      {props.course.name}
     </h1>
   )
 }
 
 const Content = (props) => {
+  console.log(props.course.parts);
   return (
-    <p>
-      <Part name={props.name1} count={props.count1} />
-      <Part name={props.name2} count={props.count2} />
-      <Part name={props.name3} count={props.count3} />
-    </p>
+    <>
+      <Part name={props.course.parts[0].name} count={props.course.parts[0].exercises} />
+      <Part name={props.course.parts[1].name} count={props.course.parts[1].exercises} />
+      <Part name={props.course.parts[2].name} count={props.course.parts[2].exercises} />
+    </ >
   )
 }
 
@@ -34,19 +35,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header name={course} />
-      <Content name1={part1} count1={exercises1} name2={part2} count2={exercises2} name3={part3} count3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
