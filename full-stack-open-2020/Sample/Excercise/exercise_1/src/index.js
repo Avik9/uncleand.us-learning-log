41b96a260aeddec181262d1026081f4aa4f6920f
Avik Kadakia <avik.kadakia@stonybrook.edu>
2020-05-21 20:21:10 -0400

    FInished 1.9

--- full-stack-open-2020/Sample/Excercise/exercise_1/src/index.js
+++ full-stack-open-2020/Sample/Excercise/exercise_1/src/index.js
@@ -1,65 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <h1>
-      {props.course.name}
-    </h1>
-  )
-}
+const Heading = props => <h1>{props.value}</h1>
+const Text = ({ text, value }) => <p>{text}{" "}{value}</p>
 
-const Content = (props) => {
-  console.log(props.course.parts);
-  return (
-    <>
-      <Part name={props.course.parts[0].name} count={props.course.parts[0].exercises} />
-      <Part name={props.course.parts[1].name} count={props.course.parts[1].exercises} />
-      <Part name={props.course.parts[2].name} count={props.course.parts[2].exercises} />
-    </ >
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} has {props.count} exercises
-    </p>
-  )
-}
+const Statistics = ({ good, neutral, bad }) => {
 
-const Total = (props) => {
-  return (
-    <p> Number of exercises: {props.total}</p>
-  )
+  if (good + bad + neutral === 0) {
+    return <Text text="No feedback given" />
+  }
+  else {
+    return (
+      <>
+        <Text text="good" value={good} />
+        <Text text="neutral" value={neutral} />
+        <Text text="bad" value={bad} />
+        <Text text="all" value={good + neutral + bad} />
+        <Text text="average" value={(good + (bad * -1)) / (good + neutral + bad)} />
+        <Text text="positive" value={((good * 100) / (good + neutral + bad)) + " %"} />
+      </>
+    )
+  }
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Heading value={"give feedback"} />
+      <p />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <p />
+      <Heading value={"statistics"} />
+      <p />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
--- full-stack-open-2020/Sample/part1/src/index.js
+++ full-stack-open-2020/Sample/part1/src/index.js
@@ -1,25 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+// const Hello = ({ name, age }) => {
+//   const bornYear = () => new Date().getFullYear() - age
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+// const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+// const History = (props) => {
+//   if (props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+
+const Display = props => <div>{props.value}</div>
+
+const App = props => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    setValue(newValue)
+  }
+
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
